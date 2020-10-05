f6c3f49607050175d43341479cc01e9a049fc2d3
dianest <nddgdd@gmail.com>
2020-10-05 23:26:10 +0200

    Initial commit

---
+++ src/main/java/ru/netology/domain/MFU.java
@@ -0,0 +1,42 @@
+package ru.netology.domain;
+
+class Printer {
+  public void print() {
+  }
+}
+
+class Scanner {
+  public void scan() {
+  }
+}
+
+
+class MFU {
+  private Printer printer;
+  private Scanner scanner;
+
+  public void print() {
+    printer.print();
+  }
+
+  public void scan() {
+    scanner.scan();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
